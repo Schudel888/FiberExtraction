0136dd946d211819e3b86ad74b7be42e991238a1
Schudel888 <les2185@columbia.edu>
2015-06-09 18:23:06 -0400

    It Works! Try it out!
    I'd advise using it on images with high thresholds only, though...

--- isolate.py
+++ isolate.py
@@ -14,7 +14,7 @@ from argparse import ArgumentDefaultsHelpFormatter
 import math
 #import os
 import sys
-#import string
+import string
 #import tempfile 
 #import shutil
 #import time 
@@ -50,20 +50,22 @@ class Point:
 class Cloud:
     '''
     def can_join(self, other):
-        return isinstance(other, Cloud) and is_adjacent(self.points, other.points) 
+        return isinstance(other, Cloud) and is_adjacent(self.Points, other.points) 
     '''
     def __init__(self, list_of_points):
         self.Points = set(filter(lambda p: isinstance(p, Point), list_of_points))
-        self.min_x = min([point.x for point in self.points])
-        self.min_y = min([point.y for point in self.points])    
-        self.mask = np.zeros( 1+max([point.x for point in self.points])-self.min_x, 1+max([point.y for point in self.points])-self.min_y)
+        self.min_x = min([point.x for point in self.Points])
+        self.min_y = min([point.y for point in self.Points])    
+        mask_shape = (1+max([point.x for point in self.Points])-self.min_x, 1+max([point.y for point in self.Points])-self.min_y)
+        self.mask = np.zeros(mask_shape)
         for point in self.Points:
-            self.mask[point.x-self.min_x][point.y-self.min_y] = point.b
+            self.mask[point.x-self.min_x][point.y-self.min_y] = float(point.b)
         '''
-        self.mask = np.zeros( 1+max([point.x for point in self.points])-self.min_x, 1+max([point.y for point in self.points])-self.min_y, *self.points[0].datashape)
-        for point in self.points:
+        self.mask = np.zeros( 1+max([point.x for point in self.Points])-self.min_x, 1+max([point.y for point in self.Points])-self.min_y, *self.Points[0].datashape)
+        for point in self.Points:
             self.mask[point.x-self.min_x][point.y-self.min_y][:] = point.data[:]
         '''
+
 
 #-----------------------------------------------------------------------------------------
 # Rough Code
@@ -113,39 +115,79 @@ def isolate_all(xyt_filename):
     Hi = data['hi'] 
     Hj = data['hj'] 
     Hthets = data['hthets']
+
+
+
+import matplotlib
+matplotlib.use('TKAgg')
+from matplotlib import pyplot as plt
+plt.ion()
+canvas = np.zeros_like(backproj)
+plt.plot(canvas)
+for cloud in list_of_Clouds:
+    canvas[cloud.min_x:][cloud.min_y:] += cloud.mask
+    plt.clf()
+    plt.plot(canvas)
+    
+#Command Line Set Refinement
+processed = list()
+while len(unprocessed) > 0:
+    sys.stdout.write('\rRefining {0} sets... '.format(len(unprocessed)))
+    sys.stdout.flush()
+    new_cloud = unprocessed.pop()
+    #no_match = True
+    matches = list()
+    #for old_cloud in unprocessed:
+    for i, old_cloud in enumerate(unprocessed):
+        if can_join(old_cloud, new_cloud):
+            old_cloud.extend(new_cloud) #old_cloud grows to include all points in new_cloud
+            #no_match = False
+            matches.append(i)
+    #if no_match:
+    if len(matches) is 0:    
+        processed.append(new_cloud)
+    else:
+        #unprocessed = map(list, map(set, unprocessed))
+        for i in matches:
+            unprocessed[i] = list(set(unprocessed[i]))
+sys.stdout.write('\rIdentified {0} clouds!'.format(len(processed)))
+sys.stdout.flush()
+print ''
+
+
 '''
 
 #-----------------------------------------------------------------------------------------
 # Bulk Fiber Isolation Functions
 #-----------------------------------------------------------------------------------------
 
-def backproj_from_xyt(xyt_filename):
+def isolate_all(xyt_filename):
+
+    #Read in RHT Output from filename_xyt??.fits
     assert xyt_filename.endswith('.fits')
     assert '_xyt' in xyt_filename
-
     print 'Accessing: '+xyt_filename+'... '
     hdu_list = fits.open(xyt_filename, mode='readonly', memmap=True, save_backup=False, checksum=True) #Allows for reading in very large files!
-    #header = hdu_list[0].header
+    header = hdu_list[0].header
     backproj = hdu_list[0].data
     #Hi = hdu_list[1].data['hi'] 
     #Hj = hdu_list[1].data['hj'] 
     #Hthets = hdu_list[1].data['hthets']
-    return backproj
 
 
-def isolate_all(backproj, can_join=is_adjacent):
-    assert backproj.ndim == 2 #2D Numpy Array 
+    #Define Adjacency Function
+    can_join=is_adjacent
 
+    #1a
+    #Command Line Set Assignment
     coords = np.nonzero(backproj) #Iterable of points
     raw_points = zip(coords[0],coords[1])
-    
     problem_size = len(raw_points)
-    print 'Assigning sets to '+str(problem_size)+' datapoints... '
-    rht.update_progress(0.0)
+    message= 'Assigning '+str(problem_size)+' datapoints to sets:'
+    rht.update_progress(0.0, message=message)
     unprocessed = list()
     while len(raw_points) > 0:
-        new_cloud = list()
-        new_cloud.append(raw_points.pop())
+        new_cloud = [raw_points.pop()] #new_cloud = list() #new_cloud.append(raw_points.pop())
         no_match = True
         for old_cloud in unprocessed:
             if can_join(old_cloud, new_cloud):
@@ -153,38 +195,34 @@ def isolate_all(backproj, can_join=is_adjacent):
                 no_match = False
         if no_match:
             unprocessed.append(new_cloud)
-        try:
-            rht.update_progress(1.0-len(raw_points)/problem_size)
-        except:
-            pass
-    try:
-        rht.update_progress(1.0)
-    except:
-        pass
-
-    print 'Refining '+str(len(unprocessed))+' sets... '
-    rht.update_progress(0.0)
+        progress = math.pow(1.0-(len(raw_points)/problem_size), 2.5) #O(n**2), but 2.5 makes it appear faster
+        if 0.0 < progress < 1.0:
+            rht.update_progress(progress=progress, message=message) 
+    unprocessed.sort(key=len, reverse=True)
+    rht.update_progress(1.0, final_message='Finished rough assignment of sets:')
+    
+    #1b
+    #Command Line Set Refinement
     processed = list()
     while len(unprocessed) > 0:
-        new_cloud = unprocessed.pop()
-        no_match = True
-        for old_cloud in unprocessed:
+        sys.stdout.write('\rRefining {0} sets... '.format(len(unprocessed)))
+        sys.stdout.flush()
+        new_cloud = unprocessed.pop(0)
+        matches = list()
+        for i, old_cloud in enumerate(unprocessed):
             if can_join(old_cloud, new_cloud):
-                old_cloud.extend(new_cloud) #old_cloud grows to include all points in new_cloud
-                no_match = False
-        if no_match:
+                matches.append(i)
+        if len(matches) is 0:    
             processed.append(new_cloud)
-        try:
-            rht.update_progress(len(processed)/(len(processed)+len(unprocessed)))
-        except:
-            pass
-    try:
-        rht.update_progress(1.0)
-    except:
-        pass
+        else:
+            matches.reverse()
+            for i in matches:
+                new_cloud.extend(unprocessed.pop(i))
+            unprocessed.append(list(set(new_cloud)))
 
-    print '# of Sets: '+str(len(processed))
-    DEMO = True
+    sys.stdout.write('\rIdentified {0} clouds!'.format(len(processed)))
+    sys.stdout.flush()
+    print ''
 
     def make_a_Point((x,y)):
         return Point(x, y, backproj[x][y])
@@ -194,25 +232,20 @@ def isolate_all(backproj, can_join=is_adjacent):
 
     list_of_Clouds = map(make_a_Cloud, processed)
 
-    if not DEMO:
-        return list_of_Clouds
+    def make_an_ImageHDU(cloud):
+        hdr = fits.Header()
+        hdr['MIN_X'] = cloud.min_x
+        hdr['MIN_X'] = cloud.min_y
+        return fits.ImageHDU(data=cloud.mask, header=hdr)
 
-    else:
-        import matplotlib
-        matplotlib.use('TKAgg')
-        from matplotlib import pyplot as plt
-        plt.ion()
-        canvas = np.zeros_like(backproj)
-        plt.plot(canvas)
-        for cloud in list_of_Clouds:
-            canvas[cloud.min_x:][cloud.min_y:] += cloud.mask
-            plt.clf()
-            plt.plot(canvas)
-        
-
-
-
-
+    list_of_HDUs = map(make_an_ImageHDU, list_of_Clouds)
+    list_of_HDUs.reverse()
+    list_of_HDUs.insert(0, hdu_list[0])
+    output_hdulist = fits.HDUList(list_of_HDUs)
+    output_filename = string.join(string.rsplit(xyt_filename, '.', 1), '_filaments.')
+    output_hdulist.writeto(output_filename, output_verify='silentfix', clobber=True, checksum=True)
+    print 'Results successfully output to '+output_filename
+    return 
         
 #-----------------------------------------------------------------------------------------
 # Command Line Mode
@@ -229,7 +262,7 @@ if __name__ == "__main__":
 
     #Do Processing
     for xyt_filename in args.files: # loop over input files
-        isolate_all(backproj_from_xyt(xyt_filename))
+        isolate_all(xyt_filename)
 
     #Cleanup and Exit
     exit()
