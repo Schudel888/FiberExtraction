719e203fcf883a3b89fb4b698af5447322c1805e
Schudel888 <les2185@columbia.edu>
2015-06-10 15:17:15 -0400

    Greatly Improved Runtime. Introduced Output File Problems..
    (One step forward but two steps back, eh?)
    
    Runtime is now on the order of O(N^2), for N=np.count_nonzero(backproj).
    
    Output files appear to contain all output masks with the proper header
    for each when read in using python, but cause my external FITS reader to
    crash. I think this is because the PrimaryHDU is copied (including the
    header, which contains checksums and such) from the input file..
    
    Added choice of can_join functions:
    is_adjacent(clouda, cloudb):
    Returns True iff any point in clouda is next to any point in cloudb
    is_within_radius(clouda, cloudb, radius):
    Returns True iff any point in clouda is within radius of any point in
    cloudb

--- isolate.py
+++ isolate.py
@@ -20,23 +20,34 @@ import string
 #import time 
 #import fnmatch
 
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import numpy as np
 import rht
+
+#-----------------------------------------------------------------------------------------
+# Initialization: Program Settings
+#-----------------------------------------------------------------------------------------
+
+SUFFIX = '_filaments.'
+
+DEBUG = True
 
 #-----------------------------------------------------------------------------------------
 # Initialization: Object Definitions
 #-----------------------------------------------------------------------------------------
 
-def combine_lists(lista, listb):
-    pass
-
-def is_adjacent(clouda, cloudb, radius=2):
+def is_within_radius(clouda, cloudb, radius=6):
     for pointa in clouda:
         for pointb in cloudb:
-            if pointa is pointb:
-                return True 
             if (math.hypot((pointa[0]-pointb[0]), (pointa[1]-pointb[1])) <= radius):
+                return True
+    return False
+
+
+def is_adjacent(clouda, cloudb):
+    for pointa in clouda:
+        for pointb in cloudb:
+            if (-1<=(pointa[0]-pointb[0])<=1 and -1<=(pointa[1]-pointb[1])<=1):
                 return True
     return False
 
@@ -166,86 +177,66 @@ def isolate_all(xyt_filename):
     #Read in RHT Output from filename_xyt??.fits
     assert xyt_filename.endswith('.fits')
     assert '_xyt' in xyt_filename
-    print 'Accessing: '+xyt_filename+'... '
+    assert SUFFIX not in xyt_filename
+    print 'Accessing: '+xyt_filename+' '
     hdu_list = fits.open(xyt_filename, mode='readonly', memmap=True, save_backup=False, checksum=True) #Allows for reading in very large files!
-    header = hdu_list[0].header
+    #header = hdu_list[0].header
     backproj = hdu_list[0].data
     #Hi = hdu_list[1].data['hi'] 
     #Hj = hdu_list[1].data['hj'] 
     #Hthets = hdu_list[1].data['hthets']
 
-
     #Define Adjacency Function
-    can_join=is_adjacent
+    can_join=is_adjacent #is_within_radius
 
-    #1a
-    #Command Line Set Assignment
-    coords = np.nonzero(backproj) #Iterable of points
-    raw_points = zip(coords[0],coords[1])
+    #Set Assignment
+    coords = np.nonzero(backproj) 
+    raw_points = zip(coords[0],coords[1]) #List of two-integer tuples
+    del coords 
     problem_size = len(raw_points)
-    message= 'Assigning '+str(problem_size)+' datapoints to sets:'
-    rht.update_progress(0.0, message=message)
+    rht.update_progress(0.0, message='Unioning '+str(problem_size)+' points:')
     unprocessed = list()
     while len(raw_points) > 0:
-        new_cloud = [raw_points.pop()] #new_cloud = list() #new_cloud.append(raw_points.pop())
-        no_match = True
-        for old_cloud in unprocessed:
-            if can_join(old_cloud, new_cloud):
-                old_cloud.extend(new_cloud) #old_cloud grows to include all points in new_cloud
-                no_match = False
-        if no_match:
-            unprocessed.append(new_cloud)
-        progress = math.pow(1.0-(len(raw_points)/problem_size), 2.5) #O(n**2), but 2.5 makes it appear faster
-        if 0.0 < progress < 1.0:
-            rht.update_progress(progress=progress, message=message) 
-    unprocessed.sort(key=len, reverse=True)
-    rht.update_progress(1.0, final_message='Finished rough assignment of sets:')
-    
-    #1b
-    #Command Line Set Refinement
-    processed = list()
-    while len(unprocessed) > 0:
-        sys.stdout.write('\rRefining {0} sets... '.format(len(unprocessed)))
-        sys.stdout.flush()
-        new_cloud = unprocessed.pop(0)
-        matches = list()
+        new_cloud = [raw_points.pop()] #List of two-integer tuples
+        matches = list() #List of integers
         for i, old_cloud in enumerate(unprocessed):
             if can_join(old_cloud, new_cloud):
                 matches.append(i)
-        if len(matches) is 0:    
-            processed.append(new_cloud)
-        else:
-            matches.reverse()
-            for i in matches:
-                new_cloud.extend(unprocessed.pop(i))
-            unprocessed.append(list(set(new_cloud)))
+        while len(matches) > 0:
+            new_cloud.extend(unprocessed.pop(matches.pop()))
+        unprocessed.append(new_cloud) #list(set(new_cloud))) #TODO DUPLICATES
+        progress = math.pow(1.0-(len(raw_points)/problem_size), 2) #O(n**2)
+        if 0.0 < progress < 1.0:
+            rht.update_progress(progress=progress, message='Unioning '+str(len(raw_points))+' points:') 
+    unprocessed.sort(key=len, reverse=True)
+    rht.update_progress(1.0, final_message='Finished unioning '+str(problem_size)+'into '+str(len(unprocessed))+' sets! Time Elapsed:')
+    
+    if DEBUG:
+        debug_data=map(len, unprocessed)
+        print debug_data
 
-    sys.stdout.write('\rIdentified {0} clouds!'.format(len(processed)))
-    sys.stdout.flush()
-    print ''
-
+    #Convert lists of two-integer tuples into ImageHDUs
     def make_a_Point((x,y)):
         return Point(x, y, backproj[x][y])
 
     def make_a_Cloud(list_of_points):
         return Cloud(map(make_a_Point, list_of_points))
 
-    list_of_Clouds = map(make_a_Cloud, processed)
-
     def make_an_ImageHDU(cloud):
         hdr = fits.Header()
         hdr['MIN_X'] = cloud.min_x
-        hdr['MIN_X'] = cloud.min_y
+        hdr['MIN_Y'] = cloud.min_y
         return fits.ImageHDU(data=cloud.mask, header=hdr)
-
-    list_of_HDUs = map(make_an_ImageHDU, list_of_Clouds)
-    list_of_HDUs.reverse()
-    list_of_HDUs.insert(0, hdu_list[0])
+    list_of_HDUs = map(make_an_ImageHDU, map(make_a_Cloud, unprocessed))
+    #list_of_HDUs.sort(key=lambda hdu: hdu.data.size, reverse=True)
+    
+    #Output HDUList to File
     output_hdulist = fits.HDUList(list_of_HDUs)
-    output_filename = string.join(string.rsplit(xyt_filename, '.', 1), '_filaments.')
+    output_hdulist.insert(0, hdu_list[0].copy()) #fits.PrimaryHDU(data=backproj, header=header)) #TODO Introduces Errors in Reading FITS File 
+    output_filename = string.join(string.rsplit(xyt_filename, '.', 1), SUFFIX)
     output_hdulist.writeto(output_filename, output_verify='silentfix', clobber=True, checksum=True)
     print 'Results successfully output to '+output_filename
-    return 
+    #return output_filename
         
 #-----------------------------------------------------------------------------------------
 # Command Line Mode
